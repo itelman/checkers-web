8e1519c33f2398675801de24dae80891aa6a600c
itelman <ilyastelman02@gmail.com>
2026-05-17 22:53:28 +0500

    upd: edited docker configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,7 @@
 # shellcheck disable=SC2046
 export $(grep -v '^#' .env | xargs)
 
-# Start Docker containers in detached mode
-docker-compose up --no-start
-
-# Run database migrations
-docker-compose start postgres
-sleep 5
-migrate -database "postgresql://${PG_USER}:${PG_PASS}@localhost:${PG_PORT}/${PG_DB}?sslmode=disable" -path ./migrations/postgres up
+docker build -t checkers .
 sleep 5
 
-docker-compose start backend
+docker run -d --name checkers -p 8888:8888 checkers
